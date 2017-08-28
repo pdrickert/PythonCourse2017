887ee1a3f04353f3e59a653fdd950a76a4ebf2d7
pdrickert <30806167+pdrickert@users.noreply.github.com>
2017-08-28 13:24:41 -0500

    Getting Better

--- Homeworks/HW5/RickertHW5.py
+++ Homeworks/HW5/RickertHW5.py
@@ -10,7 +10,7 @@ class Node:
 class LinkedList():
 
     def __init__(self, value):
-        self.head = self.Node(value)
+        self.head = Node(value)
         self.latest = self.head
         self.size = 1
         self.rev = None
@@ -22,7 +22,7 @@ class LinkedList():
 
     def addNode(self, new_value):
         if type(new_value) is not int:
-            print "PLease enter an integer"
+            print "Please enter an integer"
         if Node(new_value).value is not None:
             new_node = Node(new_value)
             newone = self.head
@@ -34,27 +34,14 @@ class LinkedList():
 
 # This is also complex at O(1) because adding a new Node doesn't increase in
 # complexity with the number of Nodes already added
-
-	def addNodeAfter(self, new_value, after_node):
-		if type(after_node) is not int:
-			print "Node number must be an integer value. I'll try and convert it for you by rounding down!"
-			try:
-				after_node = int(after_node)
-			except:
-				return "I can only convert floats and numeric strings to integers. Please try again with a valid input type."
-		if self.Node(new_value).value is not None:
-            while node.next != None:
-                tally = 1
-                node = self.head
-                while tally != after_node:
-                    tally += 1
-                    node = node.next
-                move_after = node.next
-                node.next = self.Node(new_value)
-                node.next.next = move_after
-                self.size += 1
-                return 'Added new node after node %d.' % after_node
-
+    def addNodeAfter(self, new_node, after_node):
+        if type(new_node) is not int:
+            print "Need integer"
+        else:
+            Node(new_node).next = Node(after_node).next
+            Node(after_node).next = Node(new_node)
+            self.size += 1
+            return "Node Added"
 
 # This is complex at O(n) because it has to iterate through the Nodes in order
 # to find the Node to add after, which takes up to n iterations
@@ -84,20 +71,17 @@ class LinkedList():
             self.head = self.head.next
             self.size -= 1
         else:
-                count = 1
-                Node = self.head
-                while count != Node_to_remove:
-                    count +=1
-                    Node = Node.next
-                    move_after = Node.next
-                    count = 1
-                    Node = self.head
-                    while count != Node_to_remove - 1:
-                        count += 1
-                        Node = Node.next
-                        Node.next = move_after
-        self.size -= 1
-        return "Removed Node %s" % Node_to_remove
+            count = 1
+            check = self.head
+            while count != Node_to_remove:
+                count += 1
+                check = check.next
+            while check != None:
+                check = check.next
+            check = None
+            self.size -= 1
+            return "Node Removed"
+
 # This is also complex at O(n) because it uses iterative processes. Removing those
 # would be the way to create more efficiency.
 
@@ -114,32 +98,33 @@ class LinkedList():
             return "Removed all Nodes with the value %s" % value
         else:
             self.removeNode(count)
+            self.size -= 1
             return "Node removed!"
 # This is complex at O(n^2) because you have to iterate over n Nodes n times
 
+    def findNodeBefore(self, before_node):
+        check = self.head
+        while check.next != before_node:
+            check = check.next
+        return check
 
     def reverse(self):
-        Node = self.head
-        new = self.rev
-        if Node is None:
-            self.head = self.rev
-            self.rev = None
-            return "Nodes reversed"
-        forward_count = 1
-        while Node.next is not None:
-            forward_count += 1
-            Node = Node.next
-        if new is None:
-            self.rev = Node(Node.value)
-        else:
-            reverse_count = 1
-            while new.next is not None:
-                reverse_count += 1
-                new = new.next
-            new.next = Node(Node.value)
-        self.removeNode(forward_count)
-        return self.reverse()
+        check = self.head
+        while check.next != None:
+            check = check.next
+        new_head = check
+        while check != self.head:
+            check.next = self.findNodeBefore(check)
+            check = check.next
+        self.head.next = None
+        self.head = new_head
+        print self
 # This is complex at O(n!) because you have to iterate over the entire list and
 # create a new link in each iteration, thus requiring n! calculations
-
     def __str__(self):
+        lists = "%s" %self.head
+        h = self.head
+        while 'next' in dir(h.next):
+            lists += ",%s" %(h.next)
+            h=h.next
+        return lists
